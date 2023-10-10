0c28a8d7ecb839f235845d844a0620bd4cb3186c
apple <apple@appledeMacBook-Air.local>
2023-10-10 22:20:36 +0800

    feat:color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       colors: {
         'primary-color': '#eaae36',
+        // 粉紫CC6DED 蓝48A7FA 蓝紫2C4FB0 紫944CE0 深紫2B2353
         'secondary-color': '#4b2b7a',
         'secondary-icon': '#A3A0BA',
         'card-content': '#2a3035',
@@ -14,9 +15,7 @@ module.exports = {
     }
   },
   variants: {
-    extend: {
-     
-    }
+    extend: {}
   },
   plugins: []
 }
