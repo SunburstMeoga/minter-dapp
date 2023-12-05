5170e6c41ce11e8f61bb56dae082a99d9368a76b
apple <apple@appledeMacBook-Air.local>
2023-12-05 18:54:12 +0800

    ci:contract address and contract abi

--- src/const/config.js
+++ src/const/config.js
@@ -3,18 +3,14 @@ import erc721_abi from './erc721.json'
 import game_abi from './game.json'
 
 const config = {
-  erc20_abi: erc20_abi,
-  erc721_abi: erc721_abi,
-  game_abi: game_abi,
-  service_addr: '0xCD0a551A1D0BCD1e9b2BC7C0356c7dC020ef8bca',
-  musdt_addr: '0x005A89549f5504443Ba1242259b4149e4F21ceA7',
-  pmt_addr: '0x7c2dDeFFd1717820A934d1288C0bE59247437632',
-  mt_addr: '0x167bf46afBE82A052C94F5CCfC96aA4a069Ad6eD',
-  bt_addr: '0x7B98de4BC49Fbe11f65DDcC2D91928506c6B4916',
-  rt_addr: '0x4a80819D27Afc0E4f221d993099F4c2dc3f9dCE7',
-  nft_addr: '0x2F376eC24EA6db0C55FdBb7A863c6b28701ba668',
-  game_addr: '0x8fB6289810e15b8183f032bd33426A8C8A211e82',
-  // rpc: 'https://rpc-testnet.hashahead.org',
+  '3ATUSDToken_addr': '0x53Ffa33162b1E3dd5FeE57531a6708b85266F7e0',
+  pmt_addr: '0xA6afb1F4dbC92C1019bd612D15b164AfD3caEf72',
+  mt_addr: '0xD473D757DdF59f53C77AA90eC66e4c237113935e',
+  mst_addr: '0x2D31C2AD61c7342a94eae6BBAC830819710C8425',
+  pmt_purchase_addr: '0x1a07E2f75dc58A9eAbc1e8Da5355Fc036D87Bf17',
+  nfts_collection_addr: '0xcEa698fB80d4e58F847CC4E1a2935011b15372DC',
+  nfts_marketplace_addr: '0x45977e4715d5349ba8a465aea0eF4345987b51e1',
+
   api: 'http://13.228.135.184:8000/api',
   chainId: '1688288',
   rpcUrls: ['https://rpc-testnet.hashahead.org'],
