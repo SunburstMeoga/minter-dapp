f8e4869be822569fc1f6611013a131a28753c7f3
SunburstMeoga <1025113986@qq.com>
2024-01-30 09:34:56 +0800

    fix:確認彈窗取消按鈕

--- src/components/confirm/index.js
+++ src/components/confirm/index.js
@@ -8,7 +8,7 @@ const defaultsOptions = {
   title: '提示',
   content: '内容',
   confirmText: '确定',
-  confirmText: '取消',
+  cancelText: '取消',
   confirmColor: '#E20F2A',
   isWaiting: false,
   showCancelButton: true,
