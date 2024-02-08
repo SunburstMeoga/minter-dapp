86b1dab9c8051d2a7de51ab753a5ed7aebbe436a
SunburstMeoga <1025113986@qq.com>
2024-02-08 17:44:56 +0800

    feat:language

--- src/components/TopBackBar.vue
+++ src/components/TopBackBar.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="border border-b border-gray-900">
-        <div class="flex justify-center items-center py-4 relative text-gray-200">
+        <div class="flex justify-center items-center py-6 relative text-gray-200">
             <div class="absolute  left-2 active-primary-color" @click="goBack">
                 <div class="icon iconfont icon-left" style="font-size: 20px;"></div>
             </div>
